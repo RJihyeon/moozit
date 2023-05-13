a31b4ef33675dab4bb80842e71ffc91e43c829f7
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-05-13 23:58:24 +0900

    feat: AddPost API 토큰 값 추가
    feat: AddPost API 토큰 값 추가

--- src/components/AddPost/AddPost.jsx
+++ src/components/AddPost/AddPost.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { Auth } from 'aws-amplify';
 
 import AddImage from "./AddImage";
+import { getAccessToken } from "../../utils/token";
 
 import { 
   Wrapper, 
@@ -17,6 +18,7 @@ import {
 
 
 function AddPost() {
+  const accessToken = getAccessToken([]);
   const [image, setImage ] = useState({
     imageFile: "",
     previewURL: "https://i.pinimg.com/736x/93/a6/8b/93a68b57a54e4bdc73d43d1d049b94b3.jpg",
@@ -41,6 +43,11 @@ function AddPost() {
 
   const submitForm = async () => {
     try {
+      const config = {
+        headers: {
+          'X-Amz-Security-Token': accessToken
+        }
+      };
       const userInfo = await Auth.currentUserInfo();
       const formValuesWithEmail = {
         ...formValues,
@@ -48,7 +55,7 @@ function AddPost() {
       };
       const id = Math.floor(Math.random() * 1000);
       const url = `https://ccm0e7duj5.execute-api.ap-northeast-2.amazonaws.com/dev/create-post/${id}`;
-      const data = await axios.post(url, formValuesWithEmail);
+      const data = await axios.post(url, formValuesWithEmail, config);
       console.log(data);
     } catch (e) {
       console.log(e);
