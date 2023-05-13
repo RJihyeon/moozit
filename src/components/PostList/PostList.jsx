c791fdf05dd0e751d7d67c5acec034f386604abe
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-05-13 22:40:22 +0900

    feat: drinkData.json 추가
    feat: drinkData.json 추가

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 import { Wrapper, Board, } from "./PostList.styles";
-
 import PostCard from "./PostCard";
 
 
 function PostList() {
+  const [allPost, setAllPost] = useState();
+
+  useEffect(() => {
+    getAllPost();
+  }, []);
+
+  const getAllPost = async () => {
+    try {
+      const config = {
+        headers: {
+          'X-Amz-Security-Token': '0001'
+        }
+      };
+      const data = await axios.get("https://ccm0e7duj5.execute-api.ap-northeast-2.amazonaws.com/dev/get-post-all", config);
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
     <>
