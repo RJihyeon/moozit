d819d98a6c4547c8fdea53edf3710f7ab4a0f64f
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-05-13 18:48:11 +0900

    feat: Amplify 반영
    feat: Amplify 반영

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "moozitb4236f32": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { Amplify, API, Auth } from 'aws-amplify';
+import '@aws-amplify/ui-react/styles.css';
+import './custom.css';
+import awsconfig from './aws-exports';
+import { withAuthenticator, Authenticator, } from '@aws-amplify/ui-react';
+
+Amplify.configure(awsconfig);
 
 function App() {
+
+  const services = {
+    async handleSignUp(formData) {
+      let { username, password, attributes } = formData;
+      username = username.toLowerCase();
+      attributes.email = attributes.email.toLowerCase();
+      return Auth.signUp({
+        username,
+        password,
+        attributes,
+        autoSignIn: {
+          enabled: true,
+        },
+      });
+    },
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator services={services} initialState="signUp">
+      {({ signOut }) => <button onClick={signOut}>Sign Out</button>}
+    </Authenticator>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
