aa624a22be34d2e73b33df472f7b49c7d5571d4a
RJihyeon <03x2_18@yonsei.ac.kr>
2023-05-14 00:32:51 +0900

    feat : icons 수정

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { FaRegEnvelope, FaUserAlt, FaSearch, FaPen } from 'react-icons/fa';
+import { SlHeart,SlHome } from "react-icons/sl";
+import { RxPerson } from "react-icons/rx";
 import { MenuWrapper, MenuList } from './Menu.styles';
 
 const Menu = ({ page }) => {
@@ -14,16 +15,16 @@ const Menu = ({ page }) => {
     <MenuWrapper>
       <MenuList>
         <li onClick={() => handleClick('/select')} onKeyUp={() => handleClick('/select')}>
-          <FaPen size={22} color={page === 'select' ? '#006FFD' : '#71727A'} />
-          파티 모집
+          <SlHeart size={22} color={page === 'select' ? '#006FFD' : '#71727A'} />
+          
         </li>
         <li onClick={() => handleClick('/partyList')} onKeyUp={() => handleClick('/select')}>
-          <FaSearch size={22} color={page === 'partyList' ? '#006FFD' : '#71727A'} />
-          파티 찾기
+          <SlHome size={22} color={page === 'partyList' ? '#006FFD' : '#71727A'} />
+          
         </li>
         <li onClick={() => handleClick('/mypage')} onKeyUp={() => handleClick('/select')}>
-          <FaUserAlt size={22} color={page === 'mypage' ? '#006FFD' : '#71727A'} />
-          마이 페이지
+          <RxPerson size={22} color={page === 'mypage' ? '#006FFD' : '#71727A'} />
+          
         </li>
       </MenuList>
     </MenuWrapper>
