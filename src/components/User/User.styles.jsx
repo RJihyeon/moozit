ae0e577ef8c637cbf52a3ddbda35389f4f69aead
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-05-14 01:16:19 +0900

    feat: 프로필 컴포넌트 구현중
    feat: 프로필 컴포넌트 구현중

--- src/components/User/User.styles.jsx
+++ src/components/User/User.styles.jsx
@@ -1,19 +1,65 @@
 import styled from "styled-components";
 
-
-const Body = styled.body`
-background-color: #fefafa;
-
-`;
-
 const Wrapper = styled.div`
   background-color : #fefafa;
   display: flex;
   flex-direction: column;
   width: 100%;
-  justify-content: center;
   align-items: center;
   height: calc(100vh - 4rem);
+`;
+
+const ProfileWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  padding: 1rem 2rem;
+  padding-top: 3rem;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const NameContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const NameText = styled.h2`
+  font-size: 2rem;
+  font-weight: 800;
+`;
+
+const DateText = styled.h3`
+  font-size: 1rem;
+  font-weight: 500;
+`;
+
+const ProfileImageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ProfileImage = styled.div`
+  width: 4rem;
+  height: 4rem;
+  border-radius: 50rem;
+  background-color: #42e24f;
+`;
+
+const FollowContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const FollowText = styled.p`
+  font-size: 1rem;
+  font-weight: 600;
 `;
 
 const UserInfo = styled.ul`
@@ -39,4 +85,14 @@ const UserInfo = styled.ul`
   }
 `;
 
-export {Body, Wrapper, UserInfo};
+export {
+  Wrapper,
+  ProfileWrapper, 
+  ProfileImageContainer, 
+  ProfileImage,
+  NameContainer,
+  NameText,
+  DateText,
+  FollowContainer,
+  FollowText,
+};
