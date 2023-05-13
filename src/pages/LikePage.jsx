14c7daa96f5222d12e544a761d31a47ed578b668
RJihyeon <03x2_18@yonsei.ac.kr>
2023-05-14 05:33:21 +0900

    feat: comment page 추가

--- src/pages/LikePage.jsx
+++ src/pages/LikePage.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 import Menu from "../components/Menu/Menu";
 import Margin from "../components/Margin/Margin";
+import HeadComponent from '../components/HeadComponent/HeadComponent';
+import PostList from "../components/PostList/PostList";
 
 function LikePage() {
   return (
     <>
-      <h2>
-        구현 중입니다.
-      </h2>
+      <HeadComponent/>
+      <PostList/>
       <Margin />
       <Menu page="like" />
     </>
