ceaf808e95d69e79ef2fcc1f0695e8fbc5039d48
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-05-13 19:19:09 +0900

    feat: react-router-dom
    feat: react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import '@aws-amplify/ui-react/styles.css';
 import './custom.css';
 import awsconfig from './aws-exports';
 import { withAuthenticator, Authenticator, } from '@aws-amplify/ui-react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import SplashPage from "./pages/SplashPage";
+import UserPage from "./pages/UserPage";
+import AddPostPage from "./pages/AddPostPage";
 
 Amplify.configure(awsconfig);
 
@@ -27,7 +32,13 @@ function App() {
 
   return (
     <Authenticator services={services} initialState="signUp">
-      {({ signOut }) => <button onClick={signOut}>Sign Out</button>}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SplashPage />} />
+          <Route path="/user" element={<UserPage />} />
+          <Route path="/addPost" element={<AddPostPage />} />
+        </Routes>
+      </BrowserRouter>
     </Authenticator>
   );
 }
